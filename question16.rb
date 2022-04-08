b955877dc1b2044726901a76faf1fcf1f62b97e4
MorganRohan <10355@coderacademy.edu.au>
2022-04-08 12:34:06 +1000

    final

--- question16.rb
+++ question16.rb
@@ -1,4 +1,6 @@
-#ACME Corporation are hiring a new junior developer, as part of their hiring criteria they've created a "coding skill score" based on the specific competencies they require for this role; the more important the skill is for ACME corp, the more points it contributes to the "coding skill score" 
+#ACME Corporation are hiring a new junior developer, as part of their hiring criteria they've created 
+#a "coding skill score" based on the specific competencies they require for this role; the more important 
+# the skill is for ACME corp, the more points it contributes to the "coding skill score" 
 # The skills are weighted as follows:
 # - Python (1)
 # - Ruby (2)
